08dc06a215dbee2e9bb6269f08f85bf9cbb1073d
CludeHub <149965638+CludeHub@users.noreply.github.com>
2025-05-23 18:43:17 +0800

    Update NerverLoseLibEdited.lua

--- NerverLoseLibEdited.lua
+++ NerverLoseLibEdited.lua
@@ -298,7 +298,7 @@ local framec = Instance.new("Frame")
 framec.Size = UDim2.new(0, 190, 0, 275)
 framec.Position = UDim2.new(0.78, 0, 0.15, 2) -- Center roughly
 framec.BackgroundColor3 = Color3.new(0, 0, 0)
-framec.Parent = Frame
+framec.Parent = Frame_2
 
 local corner = Instance.new("UICorner")
 corner.CornerRadius = UDim.new(0, 7)
