4f32cb90f32e694b5521978ced7e87e948551fa2
CludeHub <149965638+CludeHub@users.noreply.github.com>
2025-05-26 22:09:22 +0800

    Update NerverLoseLibEdited.lua

--- NerverLoseLibEdited.lua
+++ NerverLoseLibEdited.lua
@@ -295,7 +295,7 @@ local userId = player.UserId
 -- Create main frame
 local framec = Instance.new("Frame")
 framec.Name = "CharacterFrame"
-framec.Size = UDim2.new(-0.1, 190, 0.3819, 128)
+framec.Size = UDim2.new(-0.0636882973, 190, 0.3819, 128)
 framec.Position = UDim2.new(0.78, 0, 0.15, 2) -- Center roughly
 framec.BackgroundColor3 = Color3.new(0, 0, 0)
 framec.Parent = ScreenGui
